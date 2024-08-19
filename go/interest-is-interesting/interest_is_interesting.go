e131abc3076775cf48668b300af73efb061d696b
Fachrizal Ardiansyah <fach.ardiansyah@gmail.com>
2024-08-19 18:47:58 +0700

    Concept completed: Floating-point numbers. Exercise completed: Interest is Interesting

---
+++ go/interest-is-interesting/interest_is_interesting.go
@@ -0,0 +1,37 @@
+package interest
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	var interestRate float32
+	if balance < 0 {
+		interestRate = 3.213
+	} else if balance < 1000 {
+		interestRate = 0.5
+	} else if  balance < 5000 {
+		interestRate = 1.621
+	} else {
+		interestRate = 2.475
+	}
+	return interestRate
+}
+
+// Interest calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	return balance * float64(InterestRate(balance)/100)
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	return balance + Interest(balance)
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+	var years int
+	for i:=0; balance < targetBalance; i++{
+		years++
+		balance = AnnualBalanceUpdate(balance)
+	}
+	
+	return years
+}
